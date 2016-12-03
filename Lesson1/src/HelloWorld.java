e7f50a6f41346bb3f37967aafe78fa5273a161c2
dzhang <daniel.xuzhang@gmail.com>
2016-12-03 22:32:20 +1300

    12032016-My first class

--- Lesson1/src/HelloWorld.java
+++ Lesson1/src/HelloWorld.java
@@ -2,5 +2,22 @@
 public class HelloWorld {
     public static void main(String[] args){
     	System.out.println("Hello World!");
+    	
+    	class Student{
+    		String name;
+    		int age;
+    		float score;
+
+    		void manage(){
+    		    System.out.println(name+"'s age is: "+age+", score is: "+score);
+    		}
+    	}
+
+    	Student stu1 = new Student();
+
+    	stu1.age = 23;
+    	stu1.name = "Daniel";
+    	stu1.score = 48.56f;
+    	stu1.manage();
     }
 }
